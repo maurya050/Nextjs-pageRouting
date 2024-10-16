b6a81982b9e1e8cb5079f885e03e7ea269865ff5
Shivam Maurya <mauryashivam050@gmail.com>
2024-10-16 23:34:15 +0530

    first commit

---
+++ pages/portfolio/index.js
@@ -0,0 +1,7 @@
+function Portfolio(){
+    return <div>
+        <h1>Portfolio</h1>
+    </div>
+}
+
+export default Portfolio;
---
+++ pages/portfolio/list.js
@@ -0,0 +1,7 @@
+function ListPage(){
+    return <div>
+        <h1>Portfolio</h1>
+    </div>
+}
+
+export default ListPage;
